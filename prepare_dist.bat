b15118626ba527ae543088216b9058dd3ab31167
User33 <vip.rasdorskiy@gmail.com>
2024-06-18 19:26:50 +0500

    hf

--- prepare_dist.bat
+++ prepare_dist.bat
@@ -1,3 +1,3 @@
 docker-compose build
 docker save localhost:5000/pro-legacy_backend localhost:5000/pro-legacy_front > images.tar
-move images.tar distribution/images.tar
+move /Y images.tar install/images.tar
