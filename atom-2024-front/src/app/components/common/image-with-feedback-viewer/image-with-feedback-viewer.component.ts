dca43bf9f1b6b8a26fb8927d28fe2344776c5e4f
user32 <doomeg@yandex.ru>
2024-06-18 11:29:21 +0500

    resize & drag

--- atom-2024-front/src/app/components/common/image-with-feedback-viewer/image-with-feedback-viewer.component.ts
+++ atom-2024-front/src/app/components/common/image-with-feedback-viewer/image-with-feedback-viewer.component.ts
@@ -1,6 +1,6 @@
-import {Component, HostListener, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {Component, ElementRef, HostListener, Input, OnInit, ViewChild} from '@angular/core';
 import {TooltipModule} from 'primeng/tooltip';
-import {NgForOf, NgIf} from '@angular/common';
+import {NgForOf, NgIf, NgStyle} from '@angular/common';
 import {ImageModule} from 'primeng/image';
 import {Button} from 'primeng/button';
 import {DropdownModule} from 'primeng/dropdown';
@@ -9,7 +9,7 @@ import {OverlayPanelModule} from 'primeng/overlaypanel';
 import {InputTextModule} from 'primeng/inputtext';
 import {InputTextareaModule} from 'primeng/inputtextarea';
 import {SplitterModule} from 'primeng/splitter';
-import {AngularDraggableModule} from 'angular2-draggable';
+import {AngularDraggableModule, IPosition} from 'angular2-draggable';
 
 export enum FeedbackType {
   COMMENT = 'COMMENT', WARN = 'WARN', ERROR = 'ERROR'
@@ -31,40 +31,39 @@ export enum FeedbackType {
     InputTextModule,
     InputTextareaModule,
     SplitterModule,
-    AngularDraggableModule
+    AngularDraggableModule,
+    NgStyle
   ],
   templateUrl: './image-with-feedback-viewer.component.html',
   styleUrl: './image-with-feedback-viewer.component.css'
 })
 export class ImageWithFeedbackViewerComponent implements OnInit {
   @Input() isTutorMode = true;
-  @ViewChildren('op') overlayPanels: QueryList<OverlayPanelModule>;
+  @ViewChild('imgWrapper') imgWrapper: ElementRef;
   creatingFeedbackType: FeedbackType;
 
   scaleCoefficient = 1;
   imgSource: HTMLImageElement;
 
-  tutorSelectionMode = false;
-  tutorMoveMode = false;
+  isDrawingNow = false;
   tutorSelectionDiv: HTMLElement;
-  x1 = 0;
-  y1 = 0;
-  x2 = 0;
-  y2 = 0;
 
-  lastSelectionCoords: { x1: number, y1: number, x2: number, y2: number };
+  startCoordinates = {x1: 0, y1: 0, x2: 0, y2: 0};
+  lastCoordinates = {x1: 0, y1: 0, x2: 0, y2: 0};
   hoveredFeedback: any;
 
   feedbacks = [
     {
-      x1: 1,
-      y1: 5,
-      x2: 50,
-      y2: 40,
+      x1: 0,
+      y1: 10,
+      x2: 80,
+      y2: 100,
       type: FeedbackType.WARN,
       comment: 'Вау какой дурачок',
       isEditing: false,
-      style: {}
+      style: {},
+      initialTop: 0,
+      initialLeft: 0
     },
     {
       x1: 60,
@@ -74,7 +73,9 @@ export class ImageWithFeedbackViewerComponent implements OnInit {
       type: FeedbackType.WARN,
       comment: 'Блин реально какой-то дурачок',
       isEditing: false,
-      style: {}
+      style: {},
+      initialTop: 0,
+      initialLeft: 0
     },
     {
       x1: 200,
@@ -84,10 +85,14 @@ export class ImageWithFeedbackViewerComponent implements OnInit {
       type: FeedbackType.ERROR,
       comment: 'Вы знаете какой тут эмодзи',
       isEditing: false,
-      style: {}
+      style: {},
+      initialTop: 0,
+      initialLeft: 0
     }
   ];
 
+
+  // TODO Надо пофиксить ресайз, пока пофиг
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.calculateStyles();
@@ -103,19 +108,26 @@ export class ImageWithFeedbackViewerComponent implements OnInit {
 
   addFeedback(type: FeedbackType) {
     this.creatingFeedbackType = type;
-    this.tutorSelectionMode = true;
+    this.isDrawingNow = true;
     this.tutorSelectionDiv = document.getElementById('tutorSelection')!;
   }
 
   onImageLoad(imgSource: any) {
     this.imgSource = imgSource;
     this.calculateStyles();
+    this.setInitialOffset();
+  }
+
+  setInitialOffset() {
+    this.feedbacks.forEach(f => {
+      f.initialTop = this.getScaledPx(f.x1);
+      f.initialLeft = this.getScaledPx(f.x1);
+    });
   }
 
   calculateStyles() {
     this.scaleCoefficient = this.imgSource.clientWidth / this.imgSource.naturalWidth;
-    this.feedbacks.forEach(f => this.calculateStyle(f))
-    console.log(this.feedbacks);
+    this.feedbacks.forEach(f => this.calculateStyle(f));
   }
 
   calculateStyle(feedback: any) {
@@ -123,7 +135,8 @@ export class ImageWithFeedbackViewerComponent implements OnInit {
       left: this.getScaledPx(feedback.x1) + 'px',
       top: this.getScaledPx(feedback.y1) + 'px',
       width: this.getScaledPx(feedback.x2 - feedback.x1) + 'px',
-      height: this.getScaledPx(feedback.y2 - feedback.y1) + 'px'
+      height: this.getScaledPx(feedback.y2 - feedback.y1) + 'px',
+      transform: 'none'
     }
   }
 
@@ -135,59 +148,77 @@ export class ImageWithFeedbackViewerComponent implements OnInit {
     return scaledCoordinate / this.scaleCoefficient;
   }
 
-  reCalcTutorSelection() {
-    const x3 = Math.min(this.x1, this.x2);
-    const x4 = Math.max(this.x1, this.x2);
-    const y3 = Math.min(this.y1, this.y2);
-    const y4 = Math.max(this.y1, this.y2);
-    this.tutorSelectionDiv.style.left = x3 + 'px';
-    this.tutorSelectionDiv.style.top = y3 + 'px';
-    this.tutorSelectionDiv.style.width = x4 - x3 + 'px';
-    this.tutorSelectionDiv.style.height = y4 - y3 + 'px';
-
-    this.lastSelectionCoords = {x1: x3, y1: y3, x2: x4, y2: y4};
-  }
-
-  onMouseDown(event: MouseEvent, imgWrapper: HTMLDivElement) {
+  onDrawStart(event: MouseEvent, imgWrapper: HTMLDivElement) {
     this.stopNativeEvent(event);
-    if (!this.tutorSelectionMode) {
+    if (!this.isDrawingNow) {
       return;
     }
     this.tutorSelectionDiv.hidden = false;
-    this.x1 = event.clientX - imgWrapper.offsetLeft;
-    this.y1 = event.clientY - imgWrapper.offsetTop;
+    this.startCoordinates.x1 = event.clientX - imgWrapper.offsetLeft;
+    this.startCoordinates.y1 = event.clientY - imgWrapper.offsetTop;
     this.reCalcTutorSelection();
   }
 
-  onMouseMove(event: MouseEvent, imgWrapper: HTMLDivElement) {
-    if (!this.tutorSelectionMode) {
+  onDraw(event: MouseEvent, imgWrapper: HTMLDivElement) {
+    if (!this.isDrawingNow) {
       return;
     }
-    this.x2 = event.clientX - imgWrapper.offsetLeft;
-    this.y2 = event.clientY - imgWrapper.offsetTop;
+    this.startCoordinates.x2 = event.clientX - imgWrapper.offsetLeft;
+    this.startCoordinates.y2 = event.clientY - imgWrapper.offsetTop;
     this.reCalcTutorSelection();
   }
 
-  onMouseUp() {
-    if (!this.tutorSelectionMode) {
+  onDrawEnd() {
+    if (!this.isDrawingNow) {
       return;
     }
 
     const newFeedback = {
-      x1: this.getUnscaledPx(this.lastSelectionCoords.x1),
-      y1: this.getUnscaledPx(this.lastSelectionCoords.y1),
-      x2: this.getUnscaledPx(this.lastSelectionCoords.x2),
-      y2: this.getUnscaledPx(this.lastSelectionCoords.y2),
+      x1: this.getUnscaledPx(this.lastCoordinates.x1),
+      y1: this.getUnscaledPx(this.lastCoordinates.y1),
+      x2: this.getUnscaledPx(this.lastCoordinates.x2),
+      y2: this.getUnscaledPx(this.lastCoordinates.y2),
       type: this.creatingFeedbackType,
       style: {},
       isEditing: true,
-      comment: ''
+      comment: '',
+      initialTop: 0,
+      initialLeft: 0
     };
     this.calculateStyle(newFeedback);
 
     this.feedbacks.push(newFeedback);
     this.tutorSelectionDiv.hidden = true;
-    this.tutorSelectionMode = false;
+    this.isDrawingNow = false;
+  }
+
+  onDragEndPro(feedback: any, event: IPosition) {
+    const width = feedback.x2 - feedback.x1;
+    const height = feedback.y2 - feedback.y1;
+    feedback.x1 = Math.floor(this.getUnscaledPx(event.x + feedback.initialLeft));
+    feedback.y1 = Math.floor(this.getUnscaledPx(event.y + feedback.initialTop));
+    feedback.x2 = Math.floor(feedback.x1 + width);
+    feedback.y2 = Math.floor(feedback.y1 + height);
+
+    if (feedback.x1 < 0) {
+      feedback.x1 = 0;
+    }
+    if (feedback.y1 < 0) {
+      feedback.y1 = 0;
+    }
+  }
+
+  reCalcTutorSelection() {
+    const x3 = Math.min(this.startCoordinates.x1, this.startCoordinates.x2);
+    const x4 = Math.max(this.startCoordinates.x1, this.startCoordinates.x2);
+    const y3 = Math.min(this.startCoordinates.y1, this.startCoordinates.y2);
+    const y4 = Math.max(this.startCoordinates.y1, this.startCoordinates.y2);
+    this.tutorSelectionDiv.style.left = x3 + 'px';
+    this.tutorSelectionDiv.style.top = y3 + 'px';
+    this.tutorSelectionDiv.style.width = x4 - x3 + 'px';
+    this.tutorSelectionDiv.style.height = y4 - y3 + 'px';
+
+    this.lastCoordinates = {x1: x3, y1: y3, x2: x4, y2: y4};
   }
 
   stopNativeEvent(event: MouseEvent) {
@@ -199,32 +230,4 @@ export class ImageWithFeedbackViewerComponent implements OnInit {
   onHoverFeedbackInList(feedback: any) {
     this.hoveredFeedback = feedback;
   }
-
-  /*DRAG*/
-  onDragStart(event: MouseEvent, imgWrapper: HTMLDivElement) {
-    this.tutorMoveMode = true;
-    this.x1 = event.clientX - imgWrapper.offsetLeft;
-    this.y1 = event.clientY - imgWrapper.offsetTop;
-  }
-
-  onDragEnd(feedback: any, event: any, imgWrapper: HTMLDivElement) {
-
-    this.tutorMoveMode = false;
-
-    this.x2 = event.clientX - imgWrapper.offsetLeft;
-    this.y2 = event.clientY - imgWrapper.offsetTop;
-
-    const movedFeedback = this.feedbacks.find(f => f = feedback);
-    if (movedFeedback) {
-      const deltaX = this.getUnscaledPx(this.x2 - this.x1);
-      const deltaY = this.getUnscaledPx(this.y2 - this.y1);
-      movedFeedback.x1 = movedFeedback.x1 + deltaX;
-      movedFeedback.y1 = movedFeedback.y1 + deltaY;
-      movedFeedback.x2 = movedFeedback.x2 + deltaX;
-      movedFeedback.y2 = movedFeedback.y2 + deltaX;
-    }
-    console.log(movedFeedback);
-    this.calculateStyle(feedback);
-  }
-
 }
